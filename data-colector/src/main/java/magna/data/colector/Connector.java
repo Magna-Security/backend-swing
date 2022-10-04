ac2ce18b7ae286dd97418a0f4c511aa08841c973
matheus-rosa11 <matheus.rosa@sptech.school>
2022-10-04 20:36:08 -0300

    Adicionando formatação html ao Java Swing e corrigindo scroll bug

---
+++ data-colector/src/main/java/magna/data/colector/Connector.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package magna.data.colector;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ *
+ * @author AlfaUser
+ */
+public class Connector {
+    private JdbcTemplate connection;
+
+    public Connector() {
+        BasicDataSource dataSource = new BasicDataSource();
+
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+        dataSource.setUrl("jdbc:mysql://localhost:3306/teste");
+
+        dataSource.setUsername("root");
+
+        dataSource.setPassword("trigo1102");
+
+        this.connection = new JdbcTemplate(dataSource);
+    }
+
+    public JdbcTemplate getConnection() {
+        return connection;
+    }
+}
