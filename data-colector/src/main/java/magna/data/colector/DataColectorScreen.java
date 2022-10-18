14f061673782c2e1422ca4df0722695df2897b91
matheus-rosa11 <matheus.rosa@sptech.school>
2022-10-18 19:27:27 -0300

    teste

--- data-colector/src/main/java/magna/data/colector/DataColectorScreen.java
+++ data-colector/src/main/java/magna/data/colector/DataColectorScreen.java
@@ -109,7 +109,7 @@ public class DataColectorScreen extends javax.swing.JFrame {
         String email = txtEmail.getText();
         String senha = txtSenha.getText();
                 
-        List response = banco.queryForList("SELECT * from UsuarioSwing WHERE email = '" + email + "' and senha = '" + senha + "'");
+        List response = banco.queryForList("SELECT * from usuario WHERE email = '" + email + "' and senha = '" + senha + "'");
         
         if (response.size() > 0) {
             PosLogin frame = new PosLogin();
